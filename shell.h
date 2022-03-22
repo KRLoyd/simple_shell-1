5fad8cf64e70aacbd87031122c205dad850b73df
AnActualBanana <loganmcclain59@gmail.com>
2022-03-22 09:52:10 -0700

    Logan's first commit - header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/** libraries */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+
+
+
+
+#endif
